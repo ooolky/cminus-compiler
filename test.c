24ece4e878231641309cff219b5f59081d7d5860
ooolky <1721655584@qq.com>
2020-12-06 22:24:40 +0800

    finished

--- test.c
+++ test.c
@@ -2,27 +2,17 @@
 int a;
 char b;
 float c;
-int b;
-
-void dec(int f)
-{
-  return f;
-}
 
 void main() {
   a = 5;
   b = '1';
   c = 4.0;
 
-  f=1;
-
   while(1 > a )
   {
     if(a=3)  c--;
     if(a=2) break;
   }
-
-  decc(c);
 }
 
 
